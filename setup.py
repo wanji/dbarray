935f4a7a03c7eaba7cf7ad9aa3e0a76d77d5561d
Wan Ji <wanji@live.com>
2014-11-20 20:31:52 +0800

    add dependency to lmdb

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='dbarray',
-      version='0.1.1',
+      version='0.1.2',
       author='WAN Ji',
       author_email='wanji@live.com',
       package_dir={'dbarray': 'src'},
@@ -24,7 +24,8 @@ setup(name='dbarray',
       description='.',
       long_description=open('README.md').read(),
       install_requires=[
-          "numpy      >= 1.7.0",
-          "leveldb    >= 0.192",
+          "numpy    >= 1.7.0",
+          "leveldb  >= 0.192",
+          "lmdb     >= 0.83",
       ],
       )
